c3676f9e4479e66ac388d40719c6bae436173847
Cristina Delgado <cris3wdev@gmail.com>
2018-06-01 13:36:41 +0200

    Add example of Lambda Expressions

---
+++ src/main/java/CoreJava8/LambdaExpressionsExample.java
@@ -0,0 +1,47 @@
+package CoreJava8;
+
+import java.util.function.Function;
+
+/*
+@FunctionalInterface
+public interface Foo {
+
+    String concat(String string);
+}
+*/
+
+class FooImp {
+
+    /*
+    public String concat(String string, Foo foo) {
+        return foo.concat(string);
+    }
+    */
+
+    public String concat(String string, Function<String, String> fn) {
+        return fn.apply(string);
+    }
+}
+
+public class LambdaExpressionsExample {
+
+    public static void main(String[] args) {
+        FooImp foo = new FooImp();
+
+        String res = foo.concat("Msg ", s -> s + "from lambda");
+        System.out.println(res);
+    }
+}
+
+// Lambda Expressions and Functional Interfaces: Tips and Best Practices
+// http://www.baeldung.com/java-8-lambda-expressions-tips
+// - Prefer standard Functional Interfaces
+// - Use @FunctionalInterface annotation
+// - Don’t overuse default methods in Functional Interfaces
+// - Instantiate Functional Interfaces with Lambda Expressions
+// - Avoid overloading methods with Functional Interfaces as parameters
+// - Don’t treat Lambda Expressions as inner classes
+// - Keep Lambda Expressions short and self-explanatory
+// - Use “Effectively Final" variables
+// - Protect object Variables from mutation
+
