d7ab0b5520d86ae6dada49eaf2d3a0eb5874b90f
Cristina Delgado <cris3wdev@gmail.com>
2020-02-23 17:51:32 +0100

    Add example of ADT using enum

---
+++ src/main/java/CoreJava/Enums/CustomerType.java
@@ -0,0 +1,31 @@
+package CoreJava.Enums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum CustomerType {
+    NORMAL("NORMAL"), VIP("VIP");
+
+    private final String repr;
+
+    CustomerType(String repr) {
+        this.repr = repr;
+    }
+
+    private static final Map<String, CustomerType> stringToEnum = new HashMap<>();
+
+    static {
+        for (CustomerType customerType: values())
+            stringToEnum.put(customerType.toString(), customerType);
+    }
+
+    // returns CustomerType for string, or null if string is invalid
+    public static CustomerType fromString(String repr) {
+        return stringToEnum.get(repr);
+    }
+
+    @Override
+    public String toString() {
+        return repr;
+    }
+}
---
+++ src/test/java/CoreJava/Enums/CustomerTypeTest.java
@@ -0,0 +1,19 @@
+package CoreJava.Enums;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class CustomerTypeTest {
+
+    @Test
+    public void GivenValidStringWhenCreateEnumFromStringThenEnumIsCreated() {
+        assertEquals(CustomerType.fromString("VIP"), CustomerType.VIP);
+    }
+
+    @Test
+    public void GivenInvalidStringWhenCreateEnumFromStringThenReturnNull() {
+        assertNull(CustomerType.fromString("invalid"));
+    }
+}
