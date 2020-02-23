0731df4b0eb06b1ce63c2a0b46247783b20dba49
Cristina Delgado <cris3wdev@gmail.com>
2020-02-23 22:32:39 +0100

    Add example of Builder Pattern

---
+++ src/main/java/DesignPatterns/Creational/Builder/ComputerBuilderDemo.java
@@ -0,0 +1,15 @@
+package DesignPatterns.Creational.Builder;
+
+// https://www.journaldev.com/1425/builder-design-pattern-in-java
+
+public class ComputerBuilderDemo {
+
+    public static void main(String[] args) {
+        Computer computer =
+                new Computer
+                        .ComputerBuilder("500 GB", "2 GB")
+                        .setBluetoothEnabled(true)
+                        .setGraphicsCardEnabled(true)
+                        .build();
+    }
+}
